1e0d0f9b56fcd4d943ab2c31b378f499066c1a6c
BjarkeErichsen <s204104@dtu.dk>
2022-07-12 01:31:35 +0200

    Commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import torch
 
 
 print("hello this is another project")
@@ -10,4 +11,6 @@ print("hello this is another project")
 print("hello this is another project")
 
 print("hello king")
+
+print("hello this is another commit")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
